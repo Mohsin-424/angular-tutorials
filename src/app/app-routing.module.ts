513bccaf69253922e486c28badbda67db5a02237
Mohsinpu424 <mi873345@gmail.com>
2023-06-24 17:27:21 +0500

    main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { UserComponent } from './user/user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+// Create App Routes Here (an array of objects)
+const appRoutes: Routes = [
+  { path : '' , component:HomeComponent} ,
+   
+  // To tell the synamic route data
+  // { path : 'users/1/leela' , component:UserComponent} ,
+
+  { path : 'users', 
+  // To add path of child user components we il change it like this
+  component : UsersComponent,
+children: [
+  {path: ':id/:name' ,component: UserComponent},
+ 
+  { path: ':id/:name/edit' ,component: EditUserComponent},],
+},
+
+  { path : 'categories' , component:CategoriesComponent},
+  { path : 'not-found' ,
+   component: PageNotFoundComponent},
+  //  User here can write anything and if password does not match it will be redirected
+    { path : '**', redirectTo : ' not-found '},
+];
+ 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
+
+
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,36 @@
-import { Routes, RouterModule } from '@angular/router';
-import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+
+
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { UserComponent } from './user/user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-// Create App Routes Here (an array of objects)
-const appRoutes: Routes = [
-  { path : '' , component:HomeComponent} ,
-   
-  // To tell the synamic route data
-  // { path : 'users/1/leela' , component:UserComponent} ,
 
-  { path : 'users', 
-  // To add path of child user components we il change it like this
-  component : UsersComponent,
-children: [
-  {path: ':id/:name' ,component: UserComponent},
- 
-  { path: ':id/:name/edit' ,component: EditUserComponent},],
-},
 
-  { path : 'categories' , component:CategoriesComponent},
-  { path : 'not-found' ,
-   component: PageNotFoundComponent},
-  //  User here can write anything and if password does not match it will be redirected
-    { path : '**', redirectTo : ' not-found '},
-];
- 
+
+
 
 @NgModule({
   declarations: [
+      
     AppComponent,
-    HomeComponent,
-    UsersComponent,
-    CategoriesComponent,
-    UserComponent,
-    EditUserComponent,
-    PageNotFoundComponent
+HomeComponent ,
+UsersComponent,
+CategoriesComponent,
+UsersComponent,
+EditUserComponent,
+PageNotFoundComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
-    // Routes is an array  of javascript objects only  
+    BrowserModule,AppRoutingModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
